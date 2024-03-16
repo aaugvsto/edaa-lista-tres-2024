7b119e558af29b1812951dd722c3f9e083384ada
Augusto Assis <augustoflas@outlook.com>
2024-03-16 09:07:13 -0300

    lista 3

---
+++ src/main/java/org/app/App.java
@@ -0,0 +1,37 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package org.app;
+
+import java.util.Scanner;
+
+public class App {
+    public App() {
+    }
+
+    public static void main(String[] args) {
+
+//        System.out.println("Atividade 1:\n");
+//
+//        Atividades.Atividade1();
+
+        System.out.println("\nAtividade 2:\n");
+
+        Atividades.Atividade2();
+
+//        System.out.println("\nAtividade 3:\n");
+//
+//        Atividades.Atividade3();
+//
+//        System.out.println("\nAtividade 4:\n");
+//
+//        Atividades.Atividade4();
+//
+//        System.out.println("\nAtividade 5:\n");
+//
+//        Atividades.Atividade5();
+
+    }
+}
