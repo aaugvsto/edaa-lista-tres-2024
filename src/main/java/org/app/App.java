30608a85a2bb59b6ccd2e90a47338b4c8979d803
Augusto Assis <augustoflas@outlook.com>
2024-03-16 09:08:11 -0300

    descomentando codigo

--- src/main/java/org/app/App.java
+++ src/main/java/org/app/App.java
@@ -13,25 +13,25 @@ public class App {
 
     public static void main(String[] args) {
 
-//        System.out.println("Atividade 1:\n");
-//
-//        Atividades.Atividade1();
+        System.out.println("Atividade 1:\n");
+
+        Atividades.Atividade1();
 
         System.out.println("\nAtividade 2:\n");
 
         Atividades.Atividade2();
 
-//        System.out.println("\nAtividade 3:\n");
-//
-//        Atividades.Atividade3();
-//
-//        System.out.println("\nAtividade 4:\n");
-//
-//        Atividades.Atividade4();
-//
-//        System.out.println("\nAtividade 5:\n");
-//
-//        Atividades.Atividade5();
+        System.out.println("\nAtividade 3:\n");
+
+        Atividades.Atividade3();
+
+        System.out.println("\nAtividade 4:\n");
+
+        Atividades.Atividade4();
+
+        System.out.println("\nAtividade 5:\n");
+
+        Atividades.Atividade5();
 
     }
 }
